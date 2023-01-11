26260f899f468467addfa166226f6b9afdf784bf
taz <tazuddinleton@gmail.com>
2023-01-11 13:36:46 +0600

    Render topic tree data

---
+++ src/app/models/topic.ts
@@ -0,0 +1,19 @@
+export interface Topic {
+  id?: string;
+  key?: string;
+  label?: string;
+  data?: string;
+  selectable?: boolean;
+  children: Topic[];
+  parent?: Topic;
+}
+
+export interface TopicFlat {
+  id?: string;
+  key?: string;
+  label?: string;
+  data?: string;
+  selectable?: boolean;
+  level: number;
+  expandable: boolean;
+}
