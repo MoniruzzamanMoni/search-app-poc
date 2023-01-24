8239fa2af3303930d951b9185e72e7a43ece2eef
taz <tazuddinleton@gmail.com>
2023-01-24 18:50:50 +0600

    Bring in the template configuration for different collection type

--- src/app/models/app-config-data.ts
+++ src/app/models/app-config-data.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Dimension } from './dimension';
 import { Filter } from './filter';
 import { TopicFeatureConfig } from './topic-feature-config';
+import { TplField } from './tplField';
 
 export class AppConfigData {
   private readonly configMap: any = {};
@@ -30,12 +32,63 @@ export class AppConfigData {
     return new TopicFeatureConfig(this.configMap['topic']);
   }
 
-   public sortCollection(a: Dimension, b: Dimension): number {
+  public sortCollection(a: Dimension, b: Dimension): number {
     const collections = this.getCollections();
-    const aN = collections.findIndex(c => c['N'] === a.id);
-    const bN = collections.findIndex(c => c['N'] === b.id);
-    if (aN === bN) { return 0; }
-    if (aN > bN) { return 1; }
+    const aN = collections.findIndex((c) => c['N'] === a.id);
+    const bN = collections.findIndex((c) => c['N'] === b.id);
+    if (aN === bN) {
+      return 0;
+    }
+    if (aN > bN) {
+      return 1;
+    }
     return -1;
+  }
+
+  getCollectionResultTemplate(
+    category: number,
+    collectionCode: string
+  ): TplField[] {
+    const collection = this.getCollections().find(
+      (col) => col['N'] === category && this.hasCode(col, collectionCode)
+    );
+    const subcollection =
+      collection &&
+      collection['subcollections'] &&
+      collection['subcollections'].find((sub: any) =>
+        this.hasCode(sub, collectionCode)
+      );
+    if (subcollection && subcollection['template']) {
+      return subcollection['template']['name']
+        ? this.getTemplateByName(subcollection['template']['name'])
+        : subcollection['template'];
+    }
+    if (collection && collection['template']) {
+      return collection['template']['name']
+        ? this.getTemplateByName(collection['template']['name'])
+        : collection['template'];
+    }
+    return this.getDefaultCollectionResultTemplate();
+  }
+
+  private hasCode(collection: any, code: string): boolean {
+    if (collection['collection_code']) {
+      const found = collection['collection_code'].find((c: string) => c === code);
+      return !!found;
+    }
+    return false;
+  }
+
+  private getTemplateByName(name: string): TplField[] {
+    return this.getTemplates().find((tpl) => tpl['name'] === name)['fields'];
+  }
+
+  private getDefaultCollectionResultTemplate(): TplField[] {
+    const t = this.getCollections().find((col) => col['N'] === 0)['template'];
+    return t['name'] ? this.getTemplateByName(t['name']) : t;
+  }
+
+  private getTemplates(): any[] {
+    return this.configMap['templates'];
   }
 }
--- src/app/result-list-item/result-list-item.component.ts
+++ src/app/result-list-item/result-list-item.component.ts
@@ -1,27 +1,49 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AppConfigData } from '../models/app-config-data';
 import { EneRecord } from '../models/enerecord';
 import { EneRecordImpl } from '../models/enerecord-impl';
+import { AppConfigService } from '../services/app-config.service';
 
 @Component({
   selector: 'app-result-list-item',
   templateUrl: './result-list-item.component.html',
-  styleUrls: ['./result-list-item.component.css']
+  styleUrls: ['./result-list-item.component.css'],
 })
-export class ResultListItemComponent implements OnInit{
-
+export class ResultListItemComponent implements OnInit {
   @Input()
   result: EneRecord | undefined;
 
   categoryName = '';
   collectionName = '';
 
+  private appConfigData: AppConfigData;
+  constructor(
+    private appConfigService: AppConfigService,
+  ) {
+    this.appConfigData = new AppConfigData(this.appConfigService.config);
+  }
+
   ngOnInit() {
     const aggregate = this.result && this.result.records ? true : false;
     const recordImpl = new EneRecordImpl(this.result);
     const categoryDimension = recordImpl.getDimVal(aggregate, 'category');
-    const collection_code = recordImpl.getProperty(aggregate, 'collection_code');
+    const collection_code = recordImpl.getProperty(
+      aggregate,
+      'collection_code'
+    );
     this.categoryName = categoryDimension?.name as string;
-    this.collectionName = recordImpl.getDimVal(aggregate, 'Collection')?.name as string;
+    this.collectionName = recordImpl.getDimVal(aggregate, 'Collection')
+      ?.name as string;
+
+    const tpl = this.appConfigData.getCollectionResultTemplate(
+      categoryDimension?.id as number,
+      collection_code as string
+    );
+
+    console.log('TEMPLATE', tpl);
+
+
     console.log('Record: ', recordImpl);
   }
+
 }
