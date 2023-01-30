19847a64e6e6ef112ca75fc58381e2ef54903cd1
taz <tazuddinleton@gmail.com>
2023-01-30 12:38:29 +0600

    Load result based on search params

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { EneRecord } from '../models/enerecord';
-import { SearchEventType } from '../models/search-event/search-event';
+import { SearchEvent, SearchEventType } from '../models/search-event/search-event';
 import { SearchResult } from '../models/search-result';
 import { EndecapodService } from '../services/endecapod.service';
 import { SearchEventBusService } from '../services/search-event-bus.service';
@@ -25,25 +26,26 @@ export class ResultListComponent implements OnInit {
 
   ngOnInit() {
 
-    this.searchEventBus.on(
-      SearchEventType.AddFilter).subscribe(evt => {
+    this.searchEventBus.on()
+    .pipe(
+      filter((evt: SearchEvent) => evt.type === SearchEventType.AddFilter || evt.type === SearchEventType.RemoveFilter)
+    )
+    .subscribe(evt => {
         console.log('in search result component', evt, this.searchService.getNavigationsString())
-      });
+        this.loadResult(this.makeQuery(this.searchService.getNavigationsString()));
+    });
 
-    this.endeca.queryUrl(this.defaultQuery).subscribe(res => {
+    this.loadResult(this.makeQuery());
+  }
+
+  private loadResult(query: string) {
+    this.endeca.queryUrl(query).subscribe(res => {
       console.log('Initial result: ', res);
       this.searchResult = new SearchResult(res);
       this.records = this.searchResult.getRecords();
-      // this.searchResult.getRecords()?.forEach(rec => {
-      //     const record = new EneRecordImpl(rec);
-      //     console.log('collection', record.getDimVal(true, 'Collection'))
-      //   }
-      //   );
-      // if (this.searchResult.isAggregateQuery()) {
-      //   console.log('Aggregated');
-      // } else {
-      //   console.log('Not aggregated');
-      // }
     });
+  }
+  private makeQuery(navigations?: string): string {
+    return `N=${navigations || '0'}&Ne=7487&Nr=AND(3,10)&Nu=global_rollup_key&Np=2&Ns=sort_date_common|1`
   }
 }
